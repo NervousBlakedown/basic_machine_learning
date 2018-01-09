99e40a271cdf078ca5d6f3eede5603964087e9a1
Blake Calhoun <blakecalhoun35@gmail.com>
2018-01-09 06:22:59 -0600

    Add files via upload

---
+++ Simple Linear Regression.py
@@ -0,0 +1,30 @@
+#Simple Linear Regression.
+#Import Libraries.
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing the Dataset.
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+Y = dataset.iloc[:, 3].values
+
+#Splitting data into training set and test set.
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+#Feature scaling.
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+
+#Fitting simple linear regression to the training set.
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#Predicting the test set results.
+y_pred = regressor.predict(X_test) 
