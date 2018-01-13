0b06972a6fab52b133b1057be7c251859190f30c
Blake Calhoun <blakecalhoun35@gmail.com>
2018-01-13 11:37:24 -0600

    Add files via upload

--- Simple Linear Regression.r
+++ Simple Linear Regression.r
@@ -16,3 +16,6 @@ test_set = subset(dataset, split == FALSE)
 
 #Fitting simple linear regression to the training set
 regressor = lm(formula = Salary ~ YearsExperience, data = training_set)
+
+#Predicting the test set results
+y_pred = predict(regressor, newdata = test_set) 
