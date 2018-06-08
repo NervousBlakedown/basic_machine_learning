ee7ffaa178b889cdfbdb92b853f2371dd77b2cc0
Blake Calhoun <blakecalhoun35@gmail.com>
2018-06-08 05:17:14 -0500

    Add files via upload

--- Multiple Linear Regression.r
+++ Multiple Linear Regression.r
@@ -40,3 +40,29 @@ summary(regressor)
 regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend, # 'State' proven statistically insignificant; removed.
                 data = dataset)
 summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend, # Removed 'Administration'.
+                data = dataset)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend, # Removed 'marketing spend'.
+                data = dataset)
+summary(regressor)
+
+
+# Automatic Backward Elimination.
+backwardElimination <- function(x, sl) {
+    numVars = length(x)
+    for (i in c(1:numVars)){
+      regressor = lm(formula = Profit ~ ., data = x)
+      maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+      if (maxVar > sl){
+        j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+        x = x[, -j]
+      }
+      numVars = numVars - 1
+    }
+    return(summary(regressor))
+  }
+
+  SL = 0.05
+  dataset = dataset[, c(1,2,3,4,5)]
+  backwardElimination(training_set, SL)
