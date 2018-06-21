3f37fe4728b422baac0c665cac8e8321dfb412c9
Blake Calhoun <blakecalhoun35@gmail.com>
2018-06-21 16:58:01 -0500

    Add files via upload

---
+++ Random Forest Regression.r
@@ -0,0 +1,39 @@
+# Random Forest Regression.
+
+# Import the dataset.
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
+
+# Split dataset into training set and test set.
+# install.packages('caTools')
+# library(caTools)
+# set.seed(123)
+# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# training_set = subset(dataset, split == TRUE)
+# test_set = subset(dataset, split == FALSE)
+
+# Feature scaling.
+# training_set = scale(training_set)
+# test_set = scale(test_set)
+
+# Fit regression model to dataset.
+install.packages('randomForest')
+library(randomForest)
+set.seed(1234)
+regressor = randomForest(x = dataset[1], y = dataset$Salary, ntree = 500)
+
+# Predict new result.
+y_pred = predict(regressor, data.frame(Level = 6.5))
+
+# Visualize random forest regression results with higher resolution and smoother curve.
+install.packages('ggplot2')
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
+ggplot() +
+    geom_point(aes(x = dataset$Level, y = dataset$Salary),
+        color = 'red') +
+    geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
+        color = 'blue') +
+    ggtitle('Truth or Bluff (Random Forest Regression)') +
+    xlab('Level') +
+    ylab('Salary')
