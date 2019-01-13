9e50ba1e0fca1ffbde86e6943a9052a48fac1b06
Blake Calhoun <blakecalhoun35@gmail.com>
2019-01-13 16:29:44 -0600

    Add files via upload

---
+++ K-Means Clustering.py
@@ -0,0 +1,39 @@
+# K-Means Clustering.
+# Import packages.
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Load dataset.
+dataset = pd.read_csv('mall.csv')
+X = dataset.iloc[:, [3, 4]].values # [rows, [columns]] for parameters.
+
+# Elbow Method to find optimal number of clusters.
+from sklearn.cluster import KMeans
+wcss = []
+for i in range(1, 11):
+    kmeans = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.plot(range(1, 11), wcss)
+plt.title('The Elbow Method')
+plt.xlabel('Number of Clusters')
+plt.ylabel('WCSS')
+plt.show()
+
+# Apply k-means to dataset.
+kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualize clusters.
+plt.scatter([y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Careful')
+plt.scatter([y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Standard')
+plt.scatter([y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Target')
+plt.scatter([y_kmeans == 3, 0], X[y_kmeans == 3, 1], s = 100, c = 'cyan', label = 'Careless')
+plt.scatter([y_kmeans == 4, 0], X[y_kmeans == 4, 1], s = 100, c = 'magenta', label = 'Sensible')
+plt.scatter(kmeans.cluster_centers[:, 0], kmeans.cluster_centers[:, 1], s = 300, c = 'yellow', label = 'Centroids')
+plt.title('Clusters of Clients')
+plt.xlabel('Annual Income (k$s)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
