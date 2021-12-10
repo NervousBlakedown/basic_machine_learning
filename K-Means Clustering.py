92b32d80d086f5e85429dd0e2da94aa036052b88
Blake Calhoun <blakecalhoun@tutanota.com>
2021-12-10 15:26:01 -0500

    Update K-Means Clustering.py

--- K-Means Clustering.py
+++ K-Means Clustering.py
@@ -1,14 +1,14 @@
 # K-Means Clustering.
-# Import packages.
+# Import packages
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Load dataset.
+# Load dataset
 dataset = pd.read_csv('mall.csv')
-X = dataset.iloc[:, [3, 4]].values # [rows, [columns]] for parameters.
+X = dataset.iloc[:, [3, 4]].values # [rows, [columns]] for parameters
 
-# Elbow Method to find optimal number of clusters.
+# Elbow Method to find optimal number of clusters
 from sklearn.cluster import KMeans
 wcss = []
 for i in range(1, 11):
@@ -21,11 +21,11 @@ plt.xlabel('Number of Clusters')
 plt.ylabel('WCSS')
 plt.show()
 
-# Apply k-means to dataset.
+# Apply k-means to dataset
 kmeans = KMeans(n_clusters = 5, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0)
 y_kmeans = kmeans.fit_predict(X)
 
-# Visualize clusters.
+# Visualize clusters
 plt.scatter([y_kmeans == 0, 0], X[y_kmeans == 0, 1], s = 100, c = 'red', label = 'Careful')
 plt.scatter([y_kmeans == 1, 0], X[y_kmeans == 1, 1], s = 100, c = 'blue', label = 'Standard')
 plt.scatter([y_kmeans == 2, 0], X[y_kmeans == 2, 1], s = 100, c = 'green', label = 'Target')
